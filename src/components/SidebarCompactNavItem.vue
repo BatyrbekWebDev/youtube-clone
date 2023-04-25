94e45449db4450e97c07bcb34805ba102b4b2c63
Batyrbek <43727077+BatyrbekWebDev@users.noreply.github.com>
2023-04-25 12:27:50 +0300

    update TheDropdownSettings and TheDropdownApps

--- src/components/SidebarCompactNavItem.vue
+++ src/components/SidebarCompactNavItem.vue
@@ -1,35 +1,35 @@
 <template>
   <li>
     <a href="#" :class="classes">
-      <svg
-        class="w-6 h-6 mb-1.5"
-        fill="currentColor"
-        viewBox="0 0 20 20"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"
-        ></path>
-      </svg>
-      <span class="text-xs">Home</span>
+      <BaseIcon :name="icon" class="w-6 h-6 mb-1.5" />
+      <span class="text-xs">{{ label }}</span>
     </a>
   </li>
 </template>
 
 <script>
+import BaseIcon from './BaseIcon.vue';
+
 export default {
+  components: { BaseIcon },
+  props: {
+    isActive: Boolean,
+    label: String,
+    icon: String,
+  },
   computed: {
     classes() {
-      return [
+      const classes = [
         'flex',
         'flex-col',
         'items-center',
         'px-2',
         'py-5',
-        'text-red-500',
         'hover:bg-gray-100',
       ];
+      return this.isActive ? [...classes, 'text-red-500'] : classes
     },
   },
 };
 </script>
+<!-- Абдулмуталим -->
