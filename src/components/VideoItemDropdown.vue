9bb53fb5bb5c151915cc675dfa4281a046bd943d
Batyrbek <43727077+BatyrbekWebDev@users.noreply.github.com>
2023-04-27 17:31:06 +0300

    Выравнивание выпадающего списка по горизонтали на видео

--- src/components/VideoItemDropdown.vue
+++ src/components/VideoItemDropdown.vue
@@ -1,9 +1,6 @@
 <template>
   <div class="relative -mt-1 ml-auto">
-    <button
-      @click="isOpen = !isOpen"
-      class="-mt-1 ml-auto p-1 opacity-0 group-hover:opacity-100 text-gray-500 hover:text-gray-700 focus:outline-none"
-    >
+    <button @click="toggle" :class="buttonClasses">
       <BaseIcon name="dotsVertical" />
     </button>
     <transition
@@ -18,7 +15,7 @@
         ref="dropdown"
         @keydown.esc="isOpen = false"
         tabindex="-1"
-        class="absolute top-9 -right-full sm:right-0 bg-white w-48 rounded shadow focus:outline-none"
+        :class="dropdownClasses"
       >
         <section class="py-2">
           <ul>
@@ -41,7 +38,37 @@ export default {
   data() {
     return {
       isOpen: false,
+      positionClasses: [],
     };
+  },
+  computed: {
+    buttonClasses() {
+      return [
+        '-mt-1',
+        'ml-auto',
+        'p-1',
+        'opacity-0',
+        'group-hover:opacity-100',
+        'text-gray-500',
+        'hover:text-gray-700',
+        'focus:outline-none',
+      ];
+    },
+    dropdownClasses() {
+      return [
+        'z-10',
+        'absolute',
+        // 'top-9',
+        // '-right-full',
+        // 'sm:right-0',
+        'bg-white',
+        'w-48',
+        'rounded',
+        'shadow',
+        'focus:outline-none',
+        ...this.positionClasses,
+      ];
+    },
   },
   watch: {
     isOpen() {
@@ -54,6 +81,72 @@ export default {
         this.isOpen = false;
       }
     });
+  },
+  methods: {
+    toggle(event) {
+      this.isOpen = !this.isOpen;
+      if (this.isOpen) {
+        this.$nextTick(() => {
+          this.positionClasses = this.getPositionClasses(event);
+        });
+      }
+    },
+    getPositionClasses(event) {
+      return [
+        this.getTopClass(event),
+        this.getRightClass(event),
+        this.getLeftClass(event),
+      ];
+    },
+    getTopClass(event) {
+      const clickCoordY = event.clientY;
+      const buttonHeight = event.currentTarget.offsetHeight;
+      const dropdownHeight = this.$refs.dropdown.offsetHeight;
+
+      if (window.innerHeight - clickCoordY < dropdownHeight) {
+        return '-top-14';
+      }
+      if (window.innerHeight - clickCoordY < dropdownHeight + buttonHeight) {
+        return 'top-0';
+      }
+      return 'top-9';
+    },
+    getRightClass(event) {
+      const clickCoordX = event.clientX;
+      const clickCoordY = event.clientY;
+      const buttonHeight = event.currentTarget.offsetHeight;
+      const dropdownWidth = this.$refs.dropdown.offsetWidth;
+      const dropdownHeight = this.$refs.dropdown.offsetHeight;
+
+      if (window.innerWidth - clickCoordX > dropdownWidth) {
+        return 'right-auto';
+      }
+      if (window.innerHeight - clickCoordY > dropdownHeight + buttonHeight) {
+        return 'right-0';
+      }
+      if (window.innerHeight - clickCoordY > dropdownHeight) {
+        return 'right-8';
+      }
+      return 'right-0';
+    },
+    getLeftClass(event) {
+      const clickCoordX = event.clientX;
+      const clickCoordY = event.clientY;
+      const buttonHeight = event.currentTarget.offsetHeight;
+      const dropdownHeight = this.$refs.dropdown.offsetHeight;
+      const dropdownWidth = this.$refs.dropdown.offsetWidth;
+
+      if (window.innerWidth - clickCoordX < dropdownWidth) {
+        return 'left-auto';
+      }
+      if (window.innerHeight - clickCoordX < dropdownHeight) {
+        return 'left-auto';
+      }
+      if (window.innerHeight - clickCoordY > dropdownHeight + buttonHeight) {
+        return 'left-auto';
+      }
+      return 'left-8';
+    },
   },
 };
 </script>
