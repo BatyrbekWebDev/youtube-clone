b29c0178878695489bae935e40752607c0fdf865
Batyrbek <43727077+BatyrbekWebDev@users.noreply.github.com>
2023-04-29 22:48:46 +0300

    improvement for tooltips

--- src/components/TheSearchButton.vue
+++ src/components/TheSearchButton.vue
@@ -1,19 +1,21 @@
 <template>
-  <button
-    class="focus:outline-none flex items-center px-6 py-1 text-gray-600 bg-gray-100 border border-l-0 border-gray-300 rounded-tr-sm rounded-br-sm hover:bg-gray-200"
-  >
-    <BaseIcon name="search" class="w-5 h-5"/>
-  </button>
+  <BaseToolTip text="Search">
+    <button
+      class="h-full focus:outline-none flex items-center px-6 py-1 text-gray-600 bg-gray-100 border border-l-0 border-gray-300 rounded-tr-sm rounded-br-sm hover:bg-gray-200"
+    >
+      <BaseIcon name="search" class="w-5 h-5" />
+    </button>
+  </BaseToolTip>
 </template>
-
 
 <script>
 import BaseIcon from './BaseIcon.vue';
+import BaseToolTip from './BaseToolTip.vue';
 
-export default{
+export default {
   components: {
-    BaseIcon
-  }
-}
-
+    BaseIcon,
+    BaseToolTip,
+  },
+};
 </script>
