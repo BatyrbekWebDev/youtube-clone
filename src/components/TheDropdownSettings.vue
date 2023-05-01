9c6d3bba38d30399f630cb042f0dc59550671c47
Batyrbek <43727077+BatyrbekWebDev@users.noreply.github.com>
2023-05-01 18:30:01 +0300

    Internal menu in the drop-down list

--- src/components/TheDropdownSettings.vue
+++ src/components/TheDropdownSettings.vue
@@ -21,25 +21,14 @@
         tabindex="-1"
         :class="dropdownClasses"
       >
-        <section class="py-2 border-b">
-          <ul>
-            <DropdownSettingsListItem
-              v-for="listItem in listItems.slice(0, 8)"
-              :key="listItem.label"
-              :label="listItem.label"
-              :icon="listItem.icon"
-              :with-sub-menu="listItem.withSubMenu"
-            />
-          </ul>
-        </section>
-        <section class="py-2">
-          <ul>
-            <DropdownSettingsListItem
-              :label="listItems[8].label"
-              :with-sub-menu="listItems[8].withSubMenu"
-            />
-          </ul>
-        </section>
+        <TheDropdownSettingsMain
+          v-if="selectedMenu === 'main'"
+          @select-menu="showSelectedMenu"
+        />
+        <TheDropdownSettingsAppearance
+          v-else-if="selectedMenu === 'appearance'"
+          @select-menu="showSelectedMenu"
+        />
       </div>
     </transition>
   </div>
@@ -48,17 +37,20 @@
 <script>
 import BaseIcon from './BaseIcon.vue';
 import BaseToolTip from './BaseToolTip.vue';
-import DropdownSettingsListItem from './DropdownSettingsListItem.vue';
+import TheDropdownSettingsMain from './TheDropdownSettingsMain.vue';
+import TheDropdownSettingsAppearance from './TheDropdownSettingsAppearance.vue';
 
 export default {
   components: {
-    DropdownSettingsListItem,
     BaseIcon,
     BaseToolTip,
+    TheDropdownSettingsMain,
+    TheDropdownSettingsAppearance,
   },
   data() {
     return {
       isOpen: false,
+      selectedMenu: 'main',
       dropdownClasses: [
         'z-10',
         'absolute',
@@ -70,53 +62,6 @@ export default {
         'border',
         'border-t-0',
         'focus:outline-none',
-      ],
-      listItems: [
-        {
-          label: 'Appearance: Light',
-          icon: 'sun',
-          withSubMenu: true,
-        },
-        {
-          label: 'English',
-          icon: 'translate',
-          withSubMenu: true,
-        },
-        {
-          label: 'Ukraine',
-          icon: 'globeAlt',
-          withSubMenu: true,
-        },
-        {
-          label: 'Settings',
-          icon: 'cog',
-          withSubMenu: false,
-        },
-        {
-          label: 'Your data in YouTube',
-          icon: 'shieldCheck',
-          withSubMenu: false,
-        },
-        {
-          label: 'Help',
-          icon: 'questionMarkCircle',
-          withSubMenu: false,
-        },
-        {
-          label: 'Send Feedback',
-          icon: 'chatAlt',
-          withSubMenu: false,
-        },
-        {
-          label: 'KeyBoard shortcuts',
-          icon: 'calculate',
-          withSubMenu: false,
-        },
-        {
-          label: 'Restricted Mode: Off',
-          icon: null,
-          withSubMenu: true,
-        },
       ],
     };
   },
@@ -131,6 +76,11 @@ export default {
         this.isOpen = false;
       }
     });
+  },
+  methods: {
+    showSelectedMenu(selectedMenu) {
+      this.selectedMenu = selectedMenu;
+    },
   },
 };
 </script>
