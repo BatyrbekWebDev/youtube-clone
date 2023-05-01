269529e5790833cc6bcbe9f7c9177629c013eaa7
Batyrbek <43727077+BatyrbekWebDev@users.noreply.github.com>
2023-05-01 13:04:34 +0300

    mobile search interactivity

--- src/components/TheSearchMobile.vue
+++ src/components/TheSearchMobile.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="fixed bg-white w-full h-14 p-3 z-10 flex">
-    <BaseToolTip text="Back" right>
-      <button class="p-2 focus:outline-none">
+    <BaseToolTip @click="$emit('close')" text="Back" right>
+      <button class="mr-2 p-2 focus:outline-none">
         <BaseIcon name="arrowLeft" class="w-5 h-5" />
       </button>
     </BaseToolTip>
@@ -18,11 +18,23 @@
 import BaseIcon from './BaseIcon.vue';
 import BaseToolTip from './BaseToolTip.vue';
 import TheSearch from './TheSearch.vue';
+
 export default {
   components: {
     TheSearch,
     BaseToolTip,
     BaseIcon,
+  },
+  mounted() {
+    window.addEventListener('click', this.onClick);
+  },
+
+  methods: {
+    onClick(event) {
+      if (!this.$el.contains(event.target)) {
+        this.$emit('close');
+      }
+    },
   },
 };
 </script>
