1aa1f8e222eee2b1f1aa8d4718ddf97aef5240ad
Batyrbek <43727077+BatyrbekWebDev@users.noreply.github.com>
2023-05-15 23:05:05 +0300

    Компонент для списка поисковых подсказок

--- src/components/TheModalSearchPredictions.vue
+++ src/components/TheModalSearchPredictions.vue
@@ -1,24 +1,10 @@
 <template>
   <BaseModal class="md:w-4/5 max-w-2xl">
     <p class="text-base text-black tracking-wide">Report search predictions</p>
-    <div class="space-y-3 text-black text-sm mt-5">
-      <div
-        v-for="(label, index) in searchPredictions"
-        :key="label"
-        class="flex items-center"
-      >
-        <input
-          type="checkbox"
-          class="w-5 h-5 cursor-pointer"
-          :id="index"
-          :value="label"
-          v-model="selectedSearchPredictions"
-        />
-        <label :for="index" class="pl-4 cursor-pointer flex-grow">{{
-          label
-        }}</label>
-      </div>
-    </div>
+    <TheSearchPredictionList
+      v-model="selectedSearchPredictions"
+      :search-predictions="searchPredictions"
+    />
     <template #footer="{ close }">
       <button
         @click="close"
@@ -38,10 +24,12 @@
 
 <script>
 import BaseModal from './BaseModal.vue';
+import TheSearchPredictionList from './TheSearchPredictionList.vue';
 
 export default {
   components: {
     BaseModal,
+    TheSearchPredictionList,
   },
   data() {
     return {
