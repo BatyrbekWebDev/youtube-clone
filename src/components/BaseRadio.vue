6c3b78c0d21c2730b22961777d49e9d987e5f778
Batyrbek <43727077+BatyrbekWebDev@users.noreply.github.com>
2023-05-17 14:11:57 +0300

    Компонент радиокнопки

---
+++ src/components/BaseRadio.vue
@@ -0,0 +1,15 @@
+<template>
+  <input type="radio" class="h-5 w-5 cursor-pointer" :id="id" />
+  <label v-if="$slots.default" :for="id" class="pl-4 cursor-pointer flex-grow">
+    <slot />
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    id: String,
+    modelValue: String,
+  },
+};
+</script>
